a7dcd153b3d85077cba6e52b9b91a2d1568dd196
Kavithadinaker <kavi21oct@gmail.com>
2018-11-08 20:05:41 +0530

    Test 1

---
+++ WebApplication/Controllers/Login1Controller.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.UI.WebControls;
+
+
+namespace WebApplication.Controllers
+{
+    [EnableCorsAttribute("http://localhost:55325", "*", "*")]
+    public class Login1Controller : ApiController
+    {
+
+       [HttpPost]
+        public string Login(string Username, string Password)
+        {
+            string message = string.Empty;
+            try
+            {
+                string mainconn = ConfigurationManager.ConnectionStrings["LoginConnectionString"].ConnectionString;
+                SqlConnection sqlconn = new SqlConnection(mainconn);
+                string sqlquery = "Select USERNAME,PWD from USERTABLE where USERNAME=@USERNAME and PWD=@PWD ";
+                sqlconn.Open();
+                SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+                sqlcomm.Parameters.AddWithValue("@USERNAME", Username);
+                sqlcomm.Parameters.AddWithValue("@PWD", Password);
+                SqlDataReader sdr = sqlcomm.ExecuteReader();
+
+                if (sdr.Read())
+                {
+                    message = "Login SuccessFul";
+                }
+                else
+                {
+                    message = "Login Failed";
+                    //log4net.ILog logger = log4net.LogManager.GetLogger("ErrorLog");
+                    //logger.Error("failed");
+                }
+
+
+                sqlconn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+                //log4net.ILog logger = log4net.LogManager.GetLogger("ErrorLog");
+                //logger.Error("Meesage:" + ex.Message + "\r\n stacktrace" + ex.StackTrace);
+
+
+            }
+
+            return message;
+        }
+
+    }
+}
---
+++ WebApplication/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+
+namespace WebApplication.Controllers
+{
+    public class LoginController : ApiController
+    {
+
+        /// <summary>
+        /// Parameters username and password are obtained from database 
+        /// Sqlquery is use to check whether the details are correct or not.
+        /// Based on the value obtained from sqldata reader the condition is checked and the message is 
+        /// returned as a json type.
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <param name="Password"></param>
+        // GET: UserLogin      
+       
+        public HttpResponseMessage LoginDetails(string UserName, string Password)
+        {
+            string message = string.Empty;
+            try
+            {
+                string mainconn = ConfigurationManager.ConnectionStrings["LoginConnectionString"].ConnectionString;
+                SqlConnection sqlconn = new SqlConnection(mainconn);
+                string sqlquery = "Select USERNAME,PWD from USERTABLE where USERNAME=@USERNAME and PWD=@PWD ";
+                sqlconn.Open();
+                SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+                sqlcomm.Parameters.AddWithValue("@USERNAME", UserName);
+                sqlcomm.Parameters.AddWithValue("@PWD", Password);
+                SqlDataReader sdr = sqlcomm.ExecuteReader();
+
+                if (sdr.Read())
+                {
+                    message = "Login SuccessFul";
+                }
+                else
+                {
+                    message = "Login Failed";
+                    //log4net.ILog logger = log4net.LogManager.GetLogger("ErrorLog");
+                    //logger.Error("failed");
+
+                }
+                sqlconn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                message = "Login Failed";
+                //log4net.ILog logger = log4net.LogManager.GetLogger("ErrorLog");
+                //logger.Error("Meesage:" + ex.Message + "\r\n stacktrace" + ex.StackTrace);
+                throw (ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, message); 
+
+        }
+    }
+}
